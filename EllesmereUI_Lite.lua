3b956fc15e2499a46bede509a7b998a5554dca45
understyx <194953725+understyx@users.noreply.github.com>
2026-07-27 23:06:40 +0300

    Revert "Backport EllesmereUI to WotLK 3.3.5a"

--- EllesmereUI_Lite.lua
+++ EllesmereUI_Lite.lua
@@ -137,11 +137,7 @@ local function GetOrCreateEventFrame(addon)
     f:SetScript("OnEvent", function(self, event, ...)
         local handler = self._handlers[event]
         if handler then
-            if select("#", ...) > 0 then
-                handler(addon, event, ...)
-            else
-                handler(addon, event, _G.arg1, _G.arg2, _G.arg3, _G.arg4, _G.arg5, _G.arg6, _G.arg7, _G.arg8, _G.arg9, _G.arg10)
-            end
+            handler(addon, event, ...)
         end
     end)
     addon._eventFrame = f
