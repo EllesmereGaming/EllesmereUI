2da320486dcd6b47623aef801a5c5ccc081b89ff
svart2521 <vjekoslav.krenek@gmail.com>
2026-08-07 13:38:15 +0200

    Fix: Default UI bags support for shifter

--- EllesmereUIQoL/EllesmereUIQoL_Shifter.lua
+++ EllesmereUIQoL/EllesmereUIQoL_Shifter.lua
@@ -24,6 +24,7 @@ local eventFrame
 --  Frame registry
 -------------------------------------------------------------------------------
 local PRELOADED = {
+    "ContainerFrameCombinedBags"
     "CharacterFrame",
     "FriendsFrame",
     "PVEFrame",
@@ -104,6 +105,11 @@ local DRAG_HEADERS = {
 -- to a child frame: either a global name or a function(frame) -> child.
 local EXTRA_DRAG_TARGETS = {
     ["AchievementFrame"] = function(frame) return frame.Header or _G["AchievementFrameHeader"] end,
+    ["ContainerFrameCombinedBags"] = function(frame)
+        local t = frame.TitleContainer
+        if t and not t:IsMouseEnabled() then t:EnableMouse(true) end
+        return t
+    end,
 }
 
 -- Blizzard windows that normally dock beside CharacterFrame (Item Upgrade,
